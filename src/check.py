9fcd2e71a91898b846fbe8064352a35239ab2785
stremovsky <stremovsky@gmail.com>
2021-12-29 15:18:57 +0200

    ✍ adding slack support

--- src/check.py
+++ src/check.py
@@ -1,12 +1,27 @@
 #!/bin/python
 
-import requests
+import os
 import time
+import requests
 
-filter = ["API Requests", "Codespaces", "GitHub Packages"]
-url = "https://www.githubstatus.com/api/v2/summary.json"
+from slack_sdk import WebClient
 
-while True:
+def report_poblem(component, status):
+  slack_token = os.environ["SLACK_API_TOKEN"]
+  if slack_token is None:
+    return
+  client = WebClient(token=slack_token)
+  try:
+    response = client.chat_postMessage(
+      channel="Production",
+      text="Component: " + component+ " status: "+status
+    )
+  except Exception:
+    pass
+
+def check_status():
+  filter = ["API Requests", "Codespaces", "GitHub Packages"]
+  url = "https://www.githubstatus.com/api/v2/summary.json"
   response = requests.get(url)
   data = response.json()
   t = time.localtime()
@@ -15,5 +30,10 @@ while True:
   for component in components:
     if component["name"] in filter:
       print(current_time, component["name"], component["status"])
+      if component["status"] != "operational":
+        report_poblem(component["name"], component["status"])
+
+while True:
+  check_status()
   print("---------------------------------------------")
   time.sleep(10)
